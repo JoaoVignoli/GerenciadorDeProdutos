c8693bc954b2fc33066a837c07b5174f4308be36
JoaoVignoli <vignolijoao@gmail.com>
2025-04-14 19:47:27 -0300

    Criado base para o programa, onde o usuário deverá escolher a tarefa que deseja efetuar.

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> products = new ArrayList<>();
+        ArrayList<Double> values = new ArrayList<>();
+        ArrayList<Integer> totalOfProducts = new ArrayList<>();
+        String productName = "";
+        Double valueOfProduct = 0.0;
+        Integer totalOfProduct = 0;
+        int taskId = 0;
+        char option = 'n';
+        String separator = "===============================\n";
+        Scanner console = new Scanner(System.in);
+
+        do {
+            System.out.print(separator +
+                    "    Gerenciador de Produtos    \n" +
+                    separator +
+                    "1 - Cadastras novos produtos.\n" +
+                    "2 - Listar produtos.\n" +
+                    "3 - Alterar produto.\n" +
+                    separator +
+                    "Informe a opção desejada: ");
+            taskId = console.nextInt();
+            console.nextLine();
+
+            switch (taskId) {
+                case 1:
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                default:
+                    System.out.println("Tarefa: " + taskId + " não localizada...\n" +
+                            "Tente outra opção válida.");
+            }
+            System.out.print("Deseja realizar outra tarefa? (s/n): ");
+            option = console.next().charAt(0);
+        } while (option == 's');
+    }
+}
